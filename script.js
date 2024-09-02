226c34d5325e5a920095b6a5e6e40148ab62b430
Marco Dominguez <47532404+MarcoDomi@users.noreply.github.com>
2024-09-01 20:00:46 -0500

    create basic structure for calculator

--- script.js
+++ script.js
@@ -15,4 +15,23 @@ function divide(a, b) {
     return a / b;
 }
 
+function operate(op, num1, num2) {
+    let result;
+    switch (op) {
+        case '+':
+            result = add(num1, num2)
+            break;
+        case '-':
+            result = subtract(num1, num2)
+            break;
+        case '*':
+            result = mutiply(num1, num2)
+            break;
+        case '/':
+            result = divide(num1, num2)
+            break;
+    }
+
+    return result;
+}
 
