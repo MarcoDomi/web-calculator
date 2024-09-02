ad2a96e87f19e3611ac98985f4892ee673ded01e
Marco Dominguez <47532404+MarcoDomi@users.noreply.github.com>
2024-09-02 02:47:08 -0500

    change display value using buttons

--- script.js
+++ script.js
@@ -35,3 +35,19 @@ function operate(op, num1, num2) {
     return result;
 }
 
+function changeDisplay(element) {
+    let display = document.querySelector('#display-container');
+    let equationStr = display.textContent;
+    
+    if (equationStr === '0') {
+        display.textContent = element.textContent;
+    }
+    else {
+        display.textContent += element.textContent;
+    }
+}
+
+let display_btn = document.querySelectorAll('.display-btn');
+display_btn.forEach(element => { 
+    element.addEventListener('click', ()=>changeDisplay(element));
+});
