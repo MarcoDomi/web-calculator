afa51ad7e845733c305f33afeb0b01e793d545e9
Marco Dominguez <47532404+MarcoDomi@users.noreply.github.com>
2024-09-02 21:09:28 -0500

    finish implementing utility buttons

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 
+let display = document.querySelector('#display-container');
+
 function add(a, b) {
     return a + b;
 }
@@ -36,18 +38,43 @@ function operate(op, num1, num2) {
 }
 
 function changeDisplay(element) {
-    let display = document.querySelector('#display-container');
+    
     let equationStr = display.textContent;
     
-    if (equationStr === '0') {
+    if (equationStr === '0') 
         display.textContent = element.textContent;
-    }
-    else {
+    else 
         display.textContent += element.textContent;
-    }
 }
 
 let display_btn = document.querySelectorAll('.display-btn');
 display_btn.forEach(element => { 
     element.addEventListener('click', ()=>changeDisplay(element));
+});
+
+let decimal_btn = document.querySelector('#decimal-btn');
+decimal_btn.addEventListener('click', () => { 
+    let displayStr = display.textContent;
+
+    if (!displayStr.includes('.'))
+        display.textContent += '.';
+});
+
+
+function utility_operate(utility) {
+    switch (utility) {
+        case 'AC':
+            display.textContent = 0;
+            break;
+        case '+/-':
+            display.textContent *= -1;
+            break;
+        case '%':
+            display.textContent /= 100;
+    } 
+}
+
+let utility_btn = document.querySelectorAll('.utility-btn');
+utility_btn.forEach((element) => {
+    element.addEventListener('click',()=>utility_operate(element.textContent));
 });
